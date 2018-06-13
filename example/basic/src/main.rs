9a217abc0c47755b590c39c6cb0129c7877f4d0a
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2018-06-13 18:39:06 +0100

    Fix(Example/Basic): Change Dummy Password String To Type &str

--- example/basic/src/main.rs
+++ example/basic/src/main.rs
@@ -4,7 +4,7 @@ use std::io;
 use rustify_bruteforcer::prelude::*;
 
 fn main() -> Result<(), io::Error> {
-    let config = Config::new("AndroidAp", Some("belm2453".to_string()));
+    let config = Config::new("AndroidAp", Some("belm2453"));
     let mut bruteforcer = WifiBruteforcer::new(config)?;
 
     match bruteforcer.perform_attack()? {
